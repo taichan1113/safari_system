c55cb2561322e53bbf48833735f4628e81be4990
taichan <takecchi.1113@gmail.com>
2022-05-27 16:11:07 +0900

    merged

--- Joystick_read.py
+++ Joystick_read.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+from UDP_transmit import udptrans as Trans
 
 # pygame初期化
 pygame.init()
@@ -8,10 +9,11 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 print(f'ジョイスティック名前: {joystick.get_name()}')
 print(f'ボタン数: {joystick.get_numbuttons()}')
+print(f'ジョイスティック軸数: {joystick.get_numaxes()}')
 
 
 pygame.init()
-
+trans = Trans()
 
 while True:
   try:
@@ -20,7 +22,8 @@ while True:
         break
       
       if e.type == pygame.locals.JOYAXISMOTION:
-        print('十字キー:', joystick.get_axis(0), joystick.get_axis(1))
+        print('ジョイスティック:', joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3))
+        # trans.transmit_digits(joystick.get_axis(0))
       elif e.type == pygame.locals.JOYBUTTONDOWN:
         print(f'ボタン{e.button}を押した')
       elif e.type == pygame.locals.JOYBUTTONUP:
