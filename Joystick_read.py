71b1dcd1721aff87b74550bc9caa0f475432adac
taichan <takecchi.1113@gmail.com>
2022-05-27 19:41:45 +0900

    update UDP communication

--- Joystick_read.py
+++ Joystick_read.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from UDP_transmit import udptrans as Trans
+import time
 
 # pygame初期化
 pygame.init()
@@ -13,12 +14,15 @@ print(f'ジョイスティック軸数: {joystick.get_numaxes()}')
 
 pygame.init()
 trans = Trans()
+now = time.time()
 
 while True:
   try:
     for e in pygame.event.get():
       if e.type == QUIT:
         break
+      if time.time() - now < 0.1:
+        continue
       
       if e.type == pygame.locals.JOYAXISMOTION:
         # print('ジョイスティック:', joystick.get_axis(0), joystick.get_axis(1), joystick.get_axis(2), joystick.get_axis(3))
@@ -27,6 +31,8 @@ while True:
         print(f'ボタン{e.button}を押した')
       elif e.type == pygame.locals.JOYBUTTONUP:
         print(f'ボタン{e.button}を離した')
+      now = time.time()
+
   except KeyboardInterrupt:
     break
 
--- UDP_recieve.py
+++ UDP_recieve.py
@@ -1,7 +1,6 @@
 from socket import *
 from contextlib import closing
 import struct
-import time
 from controller.run_servo import Servo
 
 ## UDP受信クラス
@@ -15,36 +14,33 @@ class udprecv():
     self.udpServSock = socket(AF_INET, SOCK_DGRAM)
     self.udpServSock.bind(self.SrcAddr)
 
-  def recieve(self):
+  def receive(self):
     try:
       data, addr = self.udpServSock.recvfrom(self.BUFSIZE)
     except KeyboardInterrupt:
       self.udpServSock.close()
     return data, addr
-  
-  def receive_handler(self, callback):
-    data, addr = self.recieve()
-    callback(data)
 
-  def recieve_characters(self):
+  def receive_characters(self):
     while True:
-      data, addr = self.recieve()
+      data, addr = self.receive()
       print(data.decode(), addr)
 
-  def recieve_digits(self):
+  def receive_digits(self):
     while True:
-      data, addr = self.recieve()
-      print(str( struct.unpack('>d' , data)[0] ) , addr)
+      data, addr = self.receive()
+      print(str( struct.unpack('>d' , data)[0] ), addr)
+
 
 if __name__ == '__main__':
-    udp = udprecv()
-    servo = Servo()
-    servo.init()
-    # udp.recieve_digits()
-    while True:
-      data, addr = udp.recieve()
+  udp = udprecv()
+  # udp.receive_digits()
+  servo = Servo()
+  servo.init()
+  while True:
+    try:
+      data, addr = udp.receive()
       digits = struct.unpack('>d' , data)[0]
       servo.setAngle(digits*90)
-      print(f'{digits*90.0} [deg]')
-      # time.sleep(0.5)
-
+    except KeyboardInterrupt:
+      break
