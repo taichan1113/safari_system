62cfd5188b476e744ba25e7bf9a8edccb8d61661
Taichi Takemoto <takecchi.1113@gmail.com>
2022-07-26 07:03:47 +0900

    test updated

--- RC.py
+++ RC.py
@@ -8,9 +8,9 @@ dl = DataLogger()
 
 # thread_system = threading.Thread(target=tc.runActuator)
 thread_logger = threading.Thread(target=dl.log)
-thread_logger.daemon = True
-thread_logger.start()
+# thread_logger.daemon = True
+# thread_logger.start()
 
 # thread_system.start()
-# thread_logger.start()
+thread_logger.start()
 tc.runActuator()
