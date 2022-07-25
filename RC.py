2a261c89fb093bc3054a0d6d5d2d5a5cbe43ae0a
Taichi Takemoto <takecchi.1113@gmail.com>
2022-07-26 07:15:01 +0900

    test update

--- RC.py
+++ RC.py
@@ -7,10 +7,12 @@ tc = TimeConductor()
 dl = DataLogger()
 
 # thread_system = threading.Thread(target=tc.runActuator)
-thread_logger = threading.Thread(target=dl.log)
+# thread_logger = threading.Thread(target=dl.log)
 # thread_logger.daemon = True
 # thread_logger.start()
 
 # thread_system.start()
-thread_logger.start()
-tc.runActuator()
+# thread_logger.start()
+# tc.runActuator()
+
+dl.log()
