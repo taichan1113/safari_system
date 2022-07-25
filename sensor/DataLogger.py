1c505eb42512d7697f4797144f5f7dd0d1b38b39
taichan <takecchi.1113@gmail.com>
2022-07-26 07:29:22 +0900

    logger update

--- sensor/DataLogger.py
+++ sensor/DataLogger.py
@@ -12,17 +12,20 @@ class DataLogger:
     try:
       with open('test.csv', 'a', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"])
+        writer.writerow(["time", "acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"])
         print('file opened')
         self.now = time.time()
+        t = 0
         while True:
           if time.time() - self.now < self.sampling_time:
             continue
-          self.now = time.time()
           accel_data = self.sensor.get_accel_data()
           gyro_data = self.sensor.get_gyro_data()
-          data = [accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z'],]
+          data = [t, accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z'],]
           writer.writerow(data)
+          
+          t += time.time() - self.now
+          self.now = time.time()
     except KeyboardInterrupt:
       print('file created')
 
