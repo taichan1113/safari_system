3a34baec7c43ea2d4493fdad64689f97cb840cb0
taichan <takecchi.1113@gmail.com>
2022-07-26 18:56:31 +0900

    logger updated

--- RC.py
+++ RC.py
@@ -5,13 +5,12 @@ from sensor.DataLogger import DataLogger
 
 tc = TimeConductor()
 dl = DataLogger()
-
+dl.isLogging = True
 
 thread_logger = threading.Thread(target=dl.log)
 thread_logger.daemon = True
 thread_logger.start()
 
 tc.runActuator()
+dl.isLogging = False
 print('finish safely')
-
-
--- sensor/DataLogger.py
+++ sensor/DataLogger.py
@@ -2,34 +2,32 @@ import time
 import datetime
 import csv
 from sensor.mpu6050 import mpu6050
-import threading
 
-class DataLogger(threading.Thread):
+class DataLogger():
   def __init__(self):
-    super(DataLogger, self).__init__()
-    self.daemon = True
     self.sensor = mpu6050(0x69)
     self.sampling_time = 0.1
     self.now = None
+    self.isLogging = False
 
   def log(self):
-      date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-      with open(date+'.csv', 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(["time", "acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"])
-        print('file opened')
+    date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    with open(date+'.csv', 'a', newline='') as f:
+      writer = csv.writer(f)
+      writer.writerow(["time", "acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"])
+      print('file opened')
+      self.now = time.time()
+      t = 0
+      while self.isLogging:
+        if time.time() - self.now < self.sampling_time:
+          continue
+        t += time.time() - self.now
         self.now = time.time()
-        t = 0
-        while True:
-          if time.time() - self.now < self.sampling_time:
-            continue
-          t += time.time() - self.now
-          self.now = time.time()
-        
-          accel_data = self.sensor.get_accel_data()
-          gyro_data = self.sensor.get_gyro_data()
-          data = [t, accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z'],]
-          writer.writerow(data)
+      
+        accel_data = self.sensor.get_accel_data()
+        gyro_data = self.sensor.get_gyro_data()
+        data = [t, accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z'],]
+        writer.writerow(data)
 
 if __name__ == "__main__":
     logger = DataLogger()
