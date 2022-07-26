23e1d285e4e40ffc5dc749e47b1c9700e120c81d
taichan <takecchi.1113@gmail.com>
2022-07-26 16:47:59 +0900

    look better

--- plot_IMU_data.py
+++ plot_IMU_data.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pandas.read_csv('test.csv')
 
-fig, axs = plt.subplots(3,2, tight_layout=True)
+fig, axs = plt.subplots(2, 3, tight_layout=True)
 axs = axs.reshape([6,1])
 ax_names = ['acc x', 'acc y', 'acc z', 'gyro x', 'gyro y', 'gyro z']
 # ax_limits = [10, 10, 10, 100, 100, 100]
@@ -11,5 +11,5 @@ ax_names = ['acc x', 'acc y', 'acc z', 'gyro x', 'gyro y', 'gyro z']
 for i in range(6):
   axs[i,0].set_title(ax_names[i])
   # axs[i,0].set_ylim([-ax_limits[i],ax_limits[i]])
-  axs[i,0].plot(df[df.columns[i]])
+  axs[i,0].plot(df.time, df[df.columns[i+1]])
 fig.show()
