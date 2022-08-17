642461cf792be2eecc15dfd48ee7be6d18f43fe0
Taichi Takemoto <takecchi.1113@gmail.com>
2022-08-17 18:29:44 +0900

    update RC.py

--- RC.py
+++ RC.py
@@ -4,13 +4,13 @@ from models.TimeConductor import TimeConductor
 from sensor.DataLogger import DataLogger
 
 tc = TimeConductor()
-dl = DataLogger()
-dl.isLogging = True
-
-thread_logger = threading.Thread(target=dl.log)
-thread_logger.daemon = True
-thread_logger.start()
+# dl = DataLogger()
+# dl.isLogging = True
+# 
+# thread_logger = threading.Thread(target=dl.log)
+# thread_logger.daemon = True
+# thread_logger.start()
 
 tc.runActuator()
-dl.isLogging = False
+# dl.isLogging = False
 print('finish safely')
