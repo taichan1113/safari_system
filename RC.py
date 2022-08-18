48a7170063a998243bb683db05113e51288c1a22
taichan <takecchi.1113@gmail.com>
2022-08-18 16:19:48 +0900

    move features from TimeConductor to RC

--- RC.py
+++ RC.py
@@ -1,16 +1,74 @@
-# import threading
+from communication import UDP_recieve, UDP_transmit
+from models.Driving import Driving
+from models.Steering import Steering
+from sensor.Camera import Camera
+import threading
 
 from models.TimeConductor import TimeConductor
-from sensor.DataLogger import DataLogger
+# from sensor.DataLogger import DataLogger
 
-tc = TimeConductor()
+class RC:
+  def __init__(self):
+    IP = "192.168.11.11"
+    # IP = "127.0.0.1"
+    sampling_time = 0.05
+    self.tc = TimeConductor(sampling_time=sampling_time)
+
+    self.reciever = UDP_recieve.udprecv()
+    self.transmitter = UDP_transmit.udptrans(IP)
+    self.driving = Driving()
+    self.steering = Steering()
+    self.camera = Camera(1/sampling_time)
+
+  def conduct(self):
+    event = threading.Event()
+    th_actuator = threading.Thread(target=self.tc.timeKeeper, args=(self.runActuatorOnce, self.stopActuator, ))
+    th_sensor = threading.Thread(target=self.tc.timeKeeper, args=(self.transmitSensorOnce, self.closeSensor, ))
+    th_stopListner = threading.Thread(target=self.stopListening, args=(event, ))
+
+    self.isConducting = True
+
+    th_stopListner.start()
+    th_actuator.start()
+    th_sensor.start()
+    while True:
+      try:
+        continue
+      except KeyboardInterrupt:
+        break
+    event.set()
+    th_stopListner.join()
+    th_actuator.join()
+    th_sensor.join()
+    print('finish safely')
+
+  def stopListening(self, event):
+    event.wait()
+    self.isConducting = False
+
+  def runActuatorOnce(self):
+    data = self.reciever.receive_digits() # 0:steering, 1:accel, 2:break
+    self.driving.actuate([data[1], data[2]])
+    self.steering.actuate(data[0])
+
+  def stopActuator(self):
+    self.driving.stop()
+    self.steering.stop()
+    self.reciever.udpServSock.close()
+
+  def transmitSensorOnce(self):
+    frame = self.camera.capture()
+    self.transmitter.transmit_img(frame, 20)
+
+  def closeSensor(self):
+    self.camera.close()
+    self.transmitter.udpClntSock.close()
+
+
 # dl = DataLogger()
 # dl.isLogging = True
 # 
 # thread_logger = threading.Thread(target=dl.log)
 # thread_logger.daemon = True
 # thread_logger.start()
-
-tc.conduct()
 # dl.isLogging = False
-print('finish safely')
--- models/TimeConductor.py
+++ models/TimeConductor.py
@@ -1,41 +1,10 @@
 import time
-from communication import UDP_recieve, UDP_transmit
-from models.Driving import Driving
-from models.Steering import Steering
-from sensor.Camera import Camera
-import threading
 
 class TimeConductor:
-  def __init__(self):
-    IP = "192.168.11.11"
-    # IP = "127.0.0.1"
-    self.sampling_time = 0.05
+  def __init__(self, sampling_time=0.05):
+    self.sampling_time = sampling_time
     self.now = None
     self.isConducting = False
-
-    self.reciever = UDP_recieve.udprecv()
-    self.transmitter = UDP_transmit.udptrans(IP)
-    self.driving = Driving()
-    self.steering = Steering()
-    self.camera = Camera(1/self.sampling_time)
-
-  def conduct(self):
-    event = threading.Event()
-    th_actuator = threading.Thread(target=self.timeKeeper, args=(self.runActuatorOnce, self.stopActuator, ))
-    th_sensor = threading.Thread(target=self.timeKeeper, args=(self.transmitSensorOnce, self.closeSensor, ))
-    th_stopListner = threading.Thread(target=self.stopListening, args=(event, ))
-
-    self.isConducting = True
-
-    th_stopListner.start()
-    th_actuator.start()
-    th_sensor.start()
-    while True:
-      try:
-        continue
-      except KeyboardInterrupt:
-        break
-    event.set()
 
   def timeKeeper(self, executeHandler, stopHandler):
     print('start time keep')
@@ -48,28 +17,6 @@ class TimeConductor:
 
     stopHandler()
     print('finish time keep')
-
-  def stopListening(self, event):
-    event.wait()
-    self.isConducting = False
-
-  def runActuatorOnce(self):
-    data = self.reciever.receive_digits() # 0:steering, 1:accel, 2:break
-    self.driving.actuate([data[1], data[2]])
-    self.steering.actuate(data[0])
-
-  def stopActuator(self):
-    self.driving.stop()
-    self.steering.stop()
-    self.reciever.udpServSock.close()
-
-  def transmitSensorOnce(self):
-    frame = self.camera.capture()
-    self.transmitter.transmit_img(frame, 20)
-
-  def closeSensor(self):
-    self.camera.close()
-    self.transmitter.udpClntSock.close()
 
 if __name__ == '__main__':
   tc = TimeConductor()
