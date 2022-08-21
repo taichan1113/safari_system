13415dc8cd21e9fb31011ef96d724f1c491d83a7
taichan <takecchi.1113@gmail.com>
2022-08-21 22:38:21 +0900

    return TimeConductor to sleep

--- models/TimeConductor.py
+++ models/TimeConductor.py
@@ -7,14 +7,14 @@ class TimeConductor:
 
   def conduct(self, conduct_handler, finish_handler):
     print('start conduct')
-    self.now = time.time()
+    # self.now = time.time()
     while True:
       try:
-        if time.time() - self.now < self.sampling_time:
-          continue
+        # if time.time() - self.now < self.sampling_time:
+        #   continue
         conduct_handler()
-        # time.sleep(self.sampling_time)
-        self.now = time.time()
+        time.sleep(self.sampling_time)
+        # self.now = time.time()
 
       except KeyboardInterrupt:
         finish_handler()
