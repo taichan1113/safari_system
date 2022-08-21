69534993a499d39ae0a519d7ce5e68c7d6ea98b3
taichan <takecchi.1113@gmail.com>
2022-08-21 21:08:39 +0900

    time conductor update

--- models/TimeConductor.py
+++ models/TimeConductor.py
@@ -3,13 +3,18 @@ import time
 class TimeConductor:
   def __init__(self, sampling_time=0.05):
     self.sampling_time = sampling_time
+    self.now = None
 
   def conduct(self, conduct_handler, finish_handler):
     print('start conduct')
+    self.now = time.time()
     while True:
       try:
+        if time.time() - self.now < self.sampling_time:
+          continue
         conduct_handler()
-        time.sleep(self.sampling_time)
+        # time.sleep(self.sampling_time)
+        self.now = time.time()
 
       except KeyboardInterrupt:
         finish_handler()
