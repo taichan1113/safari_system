9d047c3cd114b4737984dcf5d9066f93df8053b2
taichan <takecchi.1113@gmail.com>
2022-08-24 19:20:19 +0900

    UDP client/server made

--- communication/UDP_client.py
+++ communication/UDP_client.py
@@ -1,16 +1,37 @@
 import socket
-import sys
+import numpy
+import cv2
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def getimage():
+  HOST, PORT = "localhost", 9999
+  # SOCK_DGRAM is the socket type to use for UDP sockets
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  # As you can see, there is no connect() call; UDP has no connections.
+  # Instead, data is directly sent to the recipient via sendto().
+  sock.sendto(b'test', (HOST, PORT))
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
+  # バイト型
+  # buf=b''   
+  # recvlen=100  
+  # while recvlen>0:  
+  #   # received = str(sock.recv(1024), "utf-8")
+  #   # receivedstr=sock.recv(1024)  
+  #   receivedstr=sock.recv(2**15)
+  #   recvlen=len(receivedstr)  
+  #   buf += receivedstr
+  # sock.close()
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+  byteData = sock.recv(2**19)
+  narray=numpy.fromstring(byteData, numpy.uint8)
+  img_decode = cv2.imdecode(narray, cv2.IMREAD_COLOR)
+  return img_decode
+
+while True:
+  try:
+    img = getimage()
+    # print(img)
+    cv2.imshow('result', img)
+    cv2.waitKey(1)
+  except KeyboardInterrupt:
+    print('finished')
--- communication/UDP_server.py
+++ communication/UDP_server.py
@@ -1,6 +1,9 @@
 import socketserver
+import cv2
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
+    videoCap = ''
+
     """
     This class works similar to the TCP handler class, except that
     self.request consists of a pair of data and client socket, and since
@@ -11,11 +14,20 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
         print(data)
-        socket.sendto(data.upper(), self.client_address)
+
+        # readするたびにビデオのフレームを取得
+        ret, frame = videoCap.read()
+
+        # jpegの圧縮率を設定 0～100値が高いほど高品質。何も指定しなければ95
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 30]
+
+        # 文字列に変換
+        jpegsByte = cv2.imencode('.jpeg', frame, encode_param)[1].tobytes()
+        socket.sendto(jpegsByte, self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
+    videoCap = cv2.VideoCapture(0)
     with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
         server.serve_forever()
