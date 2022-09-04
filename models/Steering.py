8bbf5ee3dee7b667e266b8dad06517f4b9c7eed8
Taichi Takemoto <takecchi.1113@gmail.com>
2022-09-04 21:07:14 +0900

    update Steering model

--- models/Steering.py
+++ models/Steering.py
@@ -2,7 +2,7 @@ from controller.run_servo_hardware import Servo
 
 class Steering:
   def __init__(self):
-    self.gear_ratio = -90.0 # ratio from data to servo
+    self.gear_ratio = -80.0 # ratio from data to servo
     self.actuator = Servo()
 #     self.actuator.init()
 
